ade0f464beccea66ec6071189c582aa5126ef78c
alvinoviando <alvinoviandoputra@gmail.com>
2021-03-31 00:04:38 +0700

    Menambah CSS di branch CSS

--- index.php
+++ index.php
@@ -3,17 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
-  
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="style1.css">
 </head>
 <body>
-  
+     <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header clearfix"> 
                         <h2 class="pull-left">Data Pelanggan</h2>
                         <a href="create.php" class="btn btn-success pull-right">Tambah Baru</a>
                     </div>
                     <?php
                     // Include config file
-                    
-                            echo "<table class='table table-bordered table-striped' >";
+                    require_once "config.php";
+
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM pelanggan";
+                    if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo "<table class='table table-bordered table-striped'>";
                                 echo "<thead>";
                                     echo "<tr>";
                                         echo "<th>ID Pelangggan</th>";
@@ -21,12 +33,37 @@
                                         echo "<th>Tanggal lahir</th>";
                                         echo "<th>Alamat</th>";
                                         echo "<th>Telepon</th>";
-                                        echo "<th>Pengaturan</th>";
+                                        echo "<th colspan = '2'>Pengaturan</th>";
                                     echo "</tr>";
                                 echo "</thead>";
                                 echo "<tbody>";
-                               
-                           
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['id'] . "</td>";
+                                        echo "<td>" . $row['nama'] . "</td>";
+                                        echo "<td>" . $row['tgl_lahir'] . "</td>";
+                                        echo "<td>" . $row['alamat'] . "</td>";
+                                        echo "<td>" . $row['telp'] . "</td>";
+                                        echo "<td>";
+                                            
+                                            echo "<a href='edit.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                            echo "<a href='hapus.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo "<p class='lead'><em>No records were found.</em></p>";
+                        }
+                    } else{
+                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                    }
+
+                    // Close connection
+                    mysqli_close($link);
                     ?>
              <!--    </div>
             </div>
